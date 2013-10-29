98f29ce997a4adad8f1eda83dd45f94475173f59
WolfLee <liyuan0228@gmail.com>
2013-10-30 00:18:38 +0800

    color page

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,6 +2,6 @@ VestaWeb.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['red', 'yellow', 'blue'];
+        return ['redere', 'yellow', 'blue'];
     }
 });
